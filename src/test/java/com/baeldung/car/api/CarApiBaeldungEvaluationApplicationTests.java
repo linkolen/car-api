27f57541856327f61aab1d79b7c295ab572b081f
taz <taz@audaxis.com>
2021-09-08 19:50:56 +0100

    init

---
+++ src/test/java/com/baeldung/car/api/CarApiBaeldungEvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung.car.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarApiBaeldungEvaluationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
